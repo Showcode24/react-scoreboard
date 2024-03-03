d65764b37a48a8f9ae3d62f98dc478b44db3239b
Showcode24 <emmanuelsoniran005@gmail.com>
2024-03-03 06:09:13 +0100

    Add Linters and header styling

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useScores = () => {
   const [scores, setScores] = useState([]);
 
   const fetchScores = () => {
     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Cdgjyo0E51CmeinsPtgM/scores/')
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         setScores(data);
-      })
-      .catch(error => {
-        console.error('Error fetching scores:', error);
       });
+    // .catch((error) => {
+    //   console.error('Error fetching scores:', error);
+    // });
   };
 
   useEffect(() => {
